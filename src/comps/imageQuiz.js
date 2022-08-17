d5462fb960a9c6cc056a5037c3a46720f9a33259
vineeth-krishna-1234 <vineethkrishna060@gmail.com>
2022-08-17 19:43:40 +0530

    updated quiz comp

--- src/comps/imageQuiz.js
+++ src/comps/imageQuiz.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import './imageQuiz.css'
+import "./imageQuiz.css";
 
 function ImageQuiz() {
-    const [randpoke,setrandpoke]=useState();
+  const [randpoke, setrandpoke] = useState();
+  const [userguess,setuserguess]=useState();
   let randomno = () => {
     return Math.floor(Math.random() * (50 - 1) + 1);
   };
@@ -14,14 +15,24 @@ function ImageQuiz() {
         .get(`https://pokeapi.co/api/v2/pokemon/${randomno()}`)
         .then((Response) => {
           console.log(Response.data.name);
-          setrandpoke(Response.data.name.toLowerCase())
+          setrandpoke(Response.data.name.toLowerCase());
         });
     };
     fetchtdata();
-  },[]);
+  }, []);
   return (
     <div>
-      <div><img src={`https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/sprites/${randpoke}.png`} id="quiz_img"/></div>
+      <div>
+        <img
+          src={`https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/sprites/${randpoke}.png`}
+          id="quiz_img"
+        />
+        <input onChange={(e)=>{
+          setuserguess(e.target.value)
+          console.log(e.target.value)
+        }}/>
+        <button>GO</button>
+      </div>
     </div>
   );
 }
